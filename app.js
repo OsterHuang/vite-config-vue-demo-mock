31ec1dfb5729c7a298363a207ebcd427e2246971
OsterHuang <oster666@gmail.com>
2024-01-19 14:57:54 +0800

    同意所有的往來源都可以通過browser cors

--- app.js
+++ app.js
@@ -15,6 +15,14 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.all('*', function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
+  res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+  
+  next()
+ });
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
